ee158693b79bd0c3083e2f5634dc6f1bfd1e0499
Athar Zafeer <atharzafeer@outlook.com>
2018-08-08 16:23:28 +0530

    will add everything that was remaining

--- js/app1.js
+++ js/app1.js
@@ -3,6 +3,13 @@ let cards=[...document.getElementsByClassName("card")];
 let deck= document.querySelector(".deck");
 const reset= document.querySelector(".restart");
 var openCards=[], viewedElements=[];
+let moves=0; 
+//let container=document.querySelector('.container');
+let stars=document.querySelector('.stars');
+console.log(stars);
+//let timer=document.querySelector('timer');
+let model=document.querySelector('#pops');
+let closebutton=document.querySelector('.close');
 /*global decalrations */
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -28,22 +35,8 @@ function start(){
         frag.appendChild(shuffledArray[i]);
     }
     deck.appendChild(frag);
-
-    let timer=document.querySelector(".timer");
-    interval = setInterval(function(){
-        timer.innerHTML = minute+"mins "+second+"secs";
-        second++;
-        
-        if(second == 60){
-            minute++;
-            second = 0;
-        }
-        if(minute == 60){
-            hour++;
-            minute = 0;
-        }
-        
-    },1000);
+    
+    
     
 }
 window.onload=start();
@@ -51,7 +44,8 @@ window.onload=start();
   const displayCard = function (){
     this.classList.toggle("open");
     this.classList.toggle("show");
-    //this.classList.toggle("disable");
+    this.classList.toggle("disable");
+    //console.log(this);
     let e=this;
     cardOpen(e);
     
@@ -63,23 +57,21 @@ function cardOpen(e)
 {   
     openCards.push(e);
     let len= openCards.length;
-    function removeElements(e,s){
-        openCards[0].classList.remove('show','open','unmatch');
-        openCards[1].classList.remove('show','open','unmatch');
-     
-        
-    }
+    
     if(len==2){
+        openCards[0].classList.toggle('disable');openCards[1].classList.toggle('disable');
         if((openCards[0].id+'1'==openCards[1].id)||(openCards[0].id==openCards[1].id+'1')){
             openCards[0].classList.add('disable');
             openCards[1].classList.add('disable');
             console.log('miss me?');
             viewedElements.push(openCards.splice(0,len));
-            
+            win();
+            movesMade(); 
         }else{
             openCards[0].classList.add('unmatch');
             openCards[1].classList.add('unmatch');
             deck.classList.add("disable");
+            movesMade();
             setTimeout(function(){
                 openCards[0].classList.remove('show','open','unmatch');
                 openCards[1].classList.remove('show','open','unmatch');
@@ -95,23 +87,41 @@ function cardOpen(e)
 
 /*card open*/
 
-reset.addEventListener('click',function(){
-   cards=shuffle(cards);
-   deck.innerHTML="";
-   let frag=document.createDocumentFragment();
-   for(let i=0;i<cards.length;i++)
-    {
-        
-        frag.appendChild(cards[i]);
-        cards[i].classList.remove("show", "open"); 
-    }
-    deck.appendChild(frag);
+reset.addEventListener('click',restart);
+function restart(){
+    setTimeout(function(){
+        cards=shuffle(cards);
+        deck.innerHTML="";
+        let frag=document.createDocumentFragment();
+        for(let i=0;i<cards.length;i++)
+         {
+             
+             frag.appendChild(cards[i]);
+             cards[i].classList.remove("show", "open","disable"); 
+         }
+         deck.appendChild(frag);
+         
+         moves=0;
+         let move=document.querySelector('.moves');
+         move.textContent=moves;
+         let timer=document.querySelector('.timer');
+         timer.textContent="0mins 0secs";
+         clearInterval(interval);
+         second=0;minute=0;hour=0;
+         stars.innerHTML=" ";
+         stars.innerHTML='<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+     },300);
+     
+}
+/* reset button */
+function startTimer(){
     let timer=document.querySelector(".timer");
     clearInterval(interval);
     timer.innerHTML="0mins 0secs"
+    minute=0,second=0;
     interval = setInterval(function(){
-        timer.innerHTML = minute+"mins "+second+"secs";
-        second++;
+    timer.innerHTML = minute+"mins "+second+"secs";
+    second++;
         
         if(second == 60){
             minute++;
@@ -123,12 +133,57 @@ reset.addEventListener('click',function(){
         }
         
     },1000);
+}
+/*timer function*/
+function movesMade(){
+    moves++;
+    let move=document.querySelector('.moves');
+    //console.log(move.textContent + moves);
+    move.textContent=moves;
+    if(moves==1){
+     startTimer();   
+    }
+    if (moves > 8 && moves < 12){
+       stars.innerHTML=""; 
+       stars.innerHTML='<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+       console.log(stars);
+    }
+    else if (moves > 12 && moves <=15){
+        stars.innerHTML="";
+        stars.innerHTML='<li><i class="fa fa-star"></i></li>';
+    }else if(moves > 15){
+        stars.innerHTML=' ';
+    }
+}
 
-});
-/* reset button */
-
-
-
+/*moves counter*/
   for (var i = 0; i < cards.length; i++){
     cards[i].addEventListener("click",displayCard,true);
  }
+/*adds eventListener*/
+function win(){
+    if(viewedElements.length==8)
+    {   
+        let timer=document.querySelector('.timer');
+        clearInterval(interval);
+        let finalTime = timer.innerHTML;
+        model.classList.add("show");
+        let starRating = document.querySelector(".stars").innerHTML;
+        document.getElementById("noOfMoves").innerHTML = moves;
+        document.getElementById("rating").innerHTML = starRating;
+        document.getElementById("time").innerHTML = finalTime;
+        //console.log(finalTime + starRating + moves);
+        model.style.cssText='visibility:visible; opacity:.7;';
+        
+        let play=document.querySelector('button');
+        play.addEventListener('click',playAgain);
+
+    }
+}
+
+
+function playAgain(){
+    model.classList.remove("show");
+    model.style.cssText="visibility:hidden; opacity:0;"
+    restart();
+}
